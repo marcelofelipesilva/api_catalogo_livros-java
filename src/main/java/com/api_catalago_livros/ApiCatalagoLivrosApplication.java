b2263bcf02f483ea38d5e787d57f98b28d8682a4
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-07-20 10:50:31 -0300

    Commint incial do projeto.

---
+++ src/main/java/com/api_catalago_livros/ApiCatalagoLivrosApplication.java
@@ -0,0 +1,13 @@
+package com.api_catalago_livros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCatalagoLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCatalagoLivrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api_catalago_livros/ApiCatalagoLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_catalago_livros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCatalagoLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
